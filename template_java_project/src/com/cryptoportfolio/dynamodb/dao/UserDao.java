fbc38cc7afab98dfb0bfc85f4152bbc3ffcaed31
daslater <davidaustinslater@protonmail.com>
2022-02-10 14:57:55 -0600

    Added Coingecko API client jar

--- template_java_project/src/com/cryptoportfolio/dynamodb/dao/UserDao.java
+++ template_java_project/src/com/cryptoportfolio/dynamodb/dao/UserDao.java
@@ -1,8 +1,8 @@
 package com.cryptoportfolio.dynamodb.dao;
 
+import com.amazon.ata.aws.dynamodb.DynamoDbClientProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.cryptoportfolio.dynamodb.DynamoDbClientProvider;
 import com.cryptoportfolio.dynamodb.models.User;
 
 public class UserDao {
