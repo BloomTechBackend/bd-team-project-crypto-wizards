344ead3efd6baf5a47ff7d02cd944808477d5478
shannonsullivan <shannons4252@gmail.com>
2022-02-15 19:46:23 -0500

    CreatePortfolio.js - Access-Control-Allow-Origin error

--- example_frontend/frontend/src/App.js
+++ example_frontend/frontend/src/App.js
@@ -6,82 +6,83 @@ import Register from "./Register";
 import Login from "./Login";
 import Portfolio from "./Portfolio";
 import {getUsername, getToken, setUserSession, resetUserSession} from '../../frontend/src/service/AuthService';
-import PublicRoute from "../../frontend/src/routes/PublicRoute";
-import PrivateRoute from "../../frontend/src/routes/PrivateRoute";
+import CreatePortfolio from "./CreatePortfolio";
 
 const verifyTokenAPIUrl = 'https://ccixqpmq4c.execute-api.us-east-2.amazonaws.com/prod/verify';
 
 function App() {
 
-  const [isAuthentication, setAuthentication] = useState('');
-  const [isTokenSet, setToken] = useState(getToken());
+    const [isAuthentication, setAuthentication] = useState(null);
+    const [isTokenSet, setToken] = useState(getToken());
 
-  useEffect(() => {
-    const token = getToken();
-    token ? setToken(true) : setToken(false);
-  })
-
-  
-  useEffect(() => {
-    const token = getToken();
-    if (token === 'undefined' || token === undefined || token === null || !token) {
-      return;
-    }
-  
-    const requestConfig = {
-      headers: {
-        'x-api-key': '9zsZhasE01a9hxGo92WUr68aGSvllMBN6Q3FHmBI'
-      }
-    }
-    const requestBody = {
-      username: getUsername(),
-      token: token
-    }
-  
-    axios.post(verifyTokenAPIUrl, requestBody, requestConfig).then((response) => {
-      setUserSession(response.data.username, response.data.token);
-      setAuthentication(false);
-    }).catch(() => {
-      resetUserSession();
-      setAuthentication(false);
+    useEffect(() => {
+        const token = getToken();
+        token ? setToken(true) : setToken(false);
     })
-  }, []);
-  
-  const token = getToken();
-  if (isAuthentication && token) {
-    return <div className="content">Authentication...</div>
-  }
 
-  // setToken(getToken());
-  return (
-      <div className="App">
-        <BrowserRouter>
-          <div className="header">
-            <NavLink className="active" to="/">Home</NavLink>
-            <NavLink className="active" to="/register">Register</NavLink>
-            <NavLink className="active" to="/login">Login</NavLink>
-            <NavLink className="active" to="/portfolio">Portfolio</NavLink>
-          </div>
-          <div className="content">
-            <Routes>
-              {/* {console.log(getToken())} */}
-              
-              {!isTokenSet && (
-                <>
-                  <Route path="/" element={<Home />}/>
-                  <Route path="/register" element={<Register/>}/>
-                  <Route path="/login" element={<Login authenticate={() => setToken(getToken())}/>}/>
-                </>
-              )}
-              {isTokenSet && (
-                <Route path="/portfolio" element={<Portfolio logout={() => setToken(getToken())}/>}/>
-              )}
-              <Route path="*" element={<Navigate to={isTokenSet ? "/portfolio" : "/"} />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-  );
+
+    useEffect(() => {
+        const token = getToken();
+        if (token === 'undefined' || token === undefined || token === null || !token) {
+            return;
+        }
+
+        const requestConfig = {
+            headers: {
+                'x-api-key': '9zsZhasE01a9hxGo92WUr68aGSvllMBN6Q3FHmBI'
+            }
+        }
+        const requestBody = {
+            username: getUsername(),
+            token: token
+        }
+
+        axios.post(verifyTokenAPIUrl, requestBody, requestConfig).then((response) => {
+            setUserSession(response.data.username, response.data.token);
+            setAuthentication(false);
+        }).catch(() => {
+            resetUserSession();
+            setAuthentication(false);
+        })
+    }, []);
+
+    const token = getToken();
+    if (isAuthentication && token) {
+        return <div className="content">Authentication...</div>
+    }
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <div className="header">
+                    <NavLink className="active" to="/">Home</NavLink>
+                    <NavLink className="active" to="/register">Register</NavLink>
+                    <NavLink className="active" to="/login">Login</NavLink>
+                    <NavLink className="active" to="/portfolio">Portfolio</NavLink>
+                </div>
+                <div className="content">
+                    <Routes>
+                        {/* {console.log(getToken())} */}
+
+                        {!isTokenSet && (
+                            <>
+                                <Route path="/" element={<Home/>}/>
+                                <Route path="/register" element={<Register/>}/>
+                                <Route path="/login" element={<Login authenticate={() => setToken(getToken())}/>}/>
+                            </>
+                        )}
+                        {isTokenSet && (
+                            <>
+                                <Route path="/portfolio" element={<Portfolio logout={() => setToken(getToken())}/>}/>
+                                <Route path="/createPortfolio" element={<CreatePortfolio/>}/>
+                            </>
+                        )}
+                        <Route path="*" element={<Navigate to={isTokenSet ? "/portfolio" : "/"}/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
