27281d84358db871aa1f2f6a027afbb3bee0f796
shannonsullivan <shannons4252@gmail.com>
2022-02-19 11:02:17 -0500

    Commit PortfolioList

---
+++ example_frontend/frontend/src/components/PortfolioList.js
@@ -0,0 +1,23 @@
+import React, {useContext, useEffect, useState} from "react";
+import PortfolioListAsset from "./PortfolioListAsset";
+
+// This component is a list of assets for CreatePortfolio,
+// Portfolio (viewPortfolio), and UpdatePortfolio
+// Pulls data from watchListContext
+const PortfolioList = ({assets, assetQuantityMap}) => {
+
+    return (
+        // Iterate over asset list in map, name element asset, then render asset
+        <ul className="coinlist list-group mt-2">
+
+            {assets.map((asset) => {
+                // Pass data as a prop to asset component for it to render
+                // key-value, Key = id  value named asset with prop passed in
+                // containing all details for id
+                return <PortfolioListAsset key={asset.id} asset={asset} assetQuantityMap={assetQuantityMap}/>;
+            })}
+        </ul>
+    );
+};
+
+export default PortfolioList;
