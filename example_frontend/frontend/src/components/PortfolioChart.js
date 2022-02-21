24ab7b4ed7a9c0fd0dc0951ec04d722efda501af
shannonsullivan <shannons4252@gmail.com>
2022-02-21 00:15:34 -0500

    PortfolioChart with hooks

--- example_frontend/frontend/src/components/PortfolioChart.js
+++ example_frontend/frontend/src/components/PortfolioChart.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useRef, useState} from "react";
 import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js';
-import { Pie } from 'react-chartjs-2';
 // Register all controllers, elements, scales, plugins from Chart.js
 import Chart from 'chart.js/auto';
 
@@ -8,7 +7,8 @@ import Chart from 'chart.js/auto';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const PortfolioChart = () => {
-
+    const chartRef = useRef();
+    const [myChart, setMyChart] = useState(null);
 
     const portfolioAssets = [
         {id: 'bitcoin', symbol: 'btc', name: 'Bitcoin', current_price: 39.875},
@@ -17,66 +17,53 @@ const PortfolioChart = () => {
         {id: 'binancecoin', symbol: 'bnb', name: 'BNB', current_price: 308.77}
     ]
 
-    let data = {
-        labels: portfolioAssets.map(asset => [asset.name, asset.id, asset.symbol].join('\r')),
+    useEffect(() => {
+        if (chartRef && chartRef.current) {
+            const myChart = new Chart(chartRef.current, {
+                type: 'pie',
+                data: {
+                    labels: portfolioAssets.map(asset => [asset.name, asset.id, asset.symbol].join('\r')),
+                    datasets: [{
+                        label: `Assets in Portfolio`,
+                        data: portfolioAssets.map(asset => [asset.current_price]),
 
-        datasets: [
-            {
-                label: `Assets in Portfolio`,
-                data: portfolioAssets.map(asset => [asset.current_price]),
-                boarderWidth: 5,
-                backgroundColor: [
-                    'rgba(255, 26, 104, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(182, 51, 204, 1)'
-                ],
-                borderColor: 'rgba(0, 0, 0, 1)',
-            },
-        ],
-    };
+                        backgroundColor: [
+                            'rgba(255, 26, 104, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(182, 51, 204, 1)'
+                        ],
+                        borderColor: 'rgba(0, 0, 0, 1)',
+                        boarderWidth: 5,
+                    }],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    scales: {},
+                    legend: {
+                        labels: {
+                            fontSize: 24,
+                        },
+                    },
+                },
+            });
+        };
+        setMyChart(myChart);
+    }, [chartRef]);
 
-    let options = {
-        maintainAspectRatio: false,
-        scales: {
-        },
-        legend: {
-            labels: {
-                fontSize: 24,
-            },
-        },
-    }
-    // let options = {
-    //
-    //     dataLabels: {
-    //         formatter: (value, context) => {
-    //
-    //             console.log(value);
-    //             console.log(context.chart.data.datasets[0].data);
-    //             const dataPoints = context.chart.data.datasets[0].data;
-    //             function totalSum(total, datapoint) {
-    //                 return total + datapoint;
-    //             }
-    //             const totalValue = dataPoints.reduce(totalSum, 0);
-    //             const percentageValue = (value / totalValue * 100).toFixed(1);
-    //             const display = [`$${value}`, `${percentageValue}%`, 'Company'];
-    //
-    //             return display;
-    //         }
-    //     }
-    // }
+    useEffect(() => {
+        if (!myChart) return;
+        myChart.update();
+    }, [myChart]);
+
     console.log("Chart");
 
     return (
-        <div className="bg-white boarder mt-2 rounded p-3" >
-            <Pie
-                data={data}
-                height={300}
-                options={options}
-            />
+        <div className="bg-white boarder mt-2 rounded p-3">
+            <canvas id="myChart" ref={chartRef} width="400" height="400"/>
         </div>
     );
 };
