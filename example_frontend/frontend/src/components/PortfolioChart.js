79a59cce247665efa46985f8ffad41961419a56d
shannonsullivan <shannons4252@gmail.com>
2022-02-22 01:51:54 -0500

    PortfolioChart implement chartInstance.destroy();

--- example_frontend/frontend/src/components/PortfolioChart.js
+++ example_frontend/frontend/src/components/PortfolioChart.js
@@ -7,21 +7,20 @@ import Chart from 'chart.js/auto';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const PortfolioChart = ({assets, assetQuantityMap}) => {
+
     const chartRef = useRef();
     const [myChart, setMyChart] = useState(null);
-
+    const _ = require('lodash');
 
     useEffect(() => {
-
         if (chartRef && chartRef.current) {
-            const myChart = new Chart(chartRef.current, {
-                type: 'pie',
+            const chartInstance = new Chart(chartRef.current, {
+                type: 'doughnut',
                 data: {
-                    labels: assets.map(asset => [asset.name, asset.id, asset.symbol].join(' \r')),
+                    labels: assets.map(asset => [asset.name, asset.symbol].join(' \r')),
                     datasets: [{
                         label: `Assets in Portfolio`,
                         data: assets.map(asset => asset.current_price * assetQuantityMap[asset.id]),
-
                         backgroundColor: [
                             'rgba(255, 26, 104, 1)',
                             'rgba(54, 162, 235, 1)',
@@ -50,12 +49,23 @@ const PortfolioChart = ({assets, assetQuantityMap}) => {
                             position: 'left',
                             align: 'center',
                             labels: {
+                                filter: (legendItem, data) => {
+                                    // Retrieve the data corresponding to that label
+                                    const label = legendItem.text
+                                    const labelIndex = _.findIndex(data.labels, (labelName) => labelName === label) // lodash used here
+                                    const qtd = data.datasets[0].data[labelIndex]
+
+                                    // Mutate the legendItem to include the new text
+                                    legendItem.text = `${legendItem.text} : ${qtd}`
+
+                                    // Filter method expects a bool, so return true to show the modified legendItem in the legend
+                                    return true
+                                },
                                 fontSize: 36,
+                                color: '#000',
                                 padding: 20,
-                                color: 'rgb(255, 99, 132)'
                             },
                         },
-
                     },
                     layout: {
                         padding: {
@@ -66,10 +76,21 @@ const PortfolioChart = ({assets, assetQuantityMap}) => {
                         },
                     },
                     tooltips: {
-                        enabled: true
+                        enabled: true,
+                        callbacks: {
+                            label: (tooltipItem, data) => {
+                                const label = data.labels[tooltipItem.index];
+                                const val =
+                                    data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+                                return label + ": $" + val;
+                            }
+                        }
                     },
                 },
             });
+            return () => {
+                chartInstance.destroy();
+            }
         };
         setMyChart(myChart);
     }, [chartRef]);
