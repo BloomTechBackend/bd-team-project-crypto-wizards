39896133ff0da64ea1d80b855e960556a2ac7deb
umasitaraman <36041143+umasitaraman@users.noreply.github.com>
2022-02-22 16:24:22 -0500

    Modified the UpdatePortfolioActivity to take in an UpdateActivityRequest and returns an UpdateActivityResponse

---
+++ template_java_project/src/com/cryptoportfolio/lambda/UpdatePortfolioActivityProvider.java
@@ -0,0 +1,24 @@
+package com.cryptoportfolio.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.cryptoportfolio.dependency.DaggerServiceComponent;
+import com.cryptoportfolio.models.requests.CreatePortfolioRequest;
+import com.cryptoportfolio.models.requests.UpdatePortfolioRequest;
+import com.cryptoportfolio.models.responses.CreatePortfolioResponse;
+import com.cryptoportfolio.models.responses.UpdatePortfolioResponse;
+
+public class UpdatePortfolioActivityProvider implements RequestHandler<UpdatePortfolioRequest, UpdatePortfolioResponse> {
+
+    public UpdatePortfolioActivityProvider() {
+
+    }
+
+    @Override
+    public UpdatePortfolioResponse handleRequest(final UpdatePortfolioRequest updatePortfolioRequest, Context context) {
+        return DaggerServiceComponent.create()
+                .provideUpdatePortfolioActivity()
+                .handleRequest(updatePortfolioRequest, context);
+    }
+
+}
