a5be2dbd090c69d7a43d0ee8debcc2206327e16a
umasitaraman <36041143+umasitaraman@users.noreply.github.com>
2022-02-25 14:46:42 -0500

    Added the no argument constructor in login request

--- template_java_project/src/com/cryptoportfolio/models/requests/LoginRequest.java
+++ template_java_project/src/com/cryptoportfolio/models/requests/LoginRequest.java
@@ -7,6 +7,10 @@ public class LoginRequest {
     private String username;
     private String password;
 
+    public LoginRequest() {
+
+    }
+
     public LoginRequest(String username, String password) {
         this.username = username;
         this.password = password;
